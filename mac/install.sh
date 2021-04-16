743f6cd0c48365bc86d776dd472881012302181e
Ion Caza <let@caza.in>
2021-04-15 20:41:45 -0400

    Mac installer, working now?

--- mac/install.sh
+++ mac/install.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
 ## Run as sudo
-chmod 0755 ionskvm-mac.sh
+rm /usr/local/bin/ionskvm-mac.sh
 cp ionskvm-mac.sh /usr/local/bin
+chmod 0755 /usr/local/bin/ionskvm-mac.sh
+rm -rf /usr/local/bin/lgtvcontrol
 mkdir /usr/local/bin/lgtvcontrol
 git clone https://github.com/IonCaza/ionskvm /usr/local/bin/lgtvcontrol
-npm install /usr/local/bin/lgtvcontrol
+oldpath=`pwd`
+cd /usr/local/bin
+npm install
 
 # xpc build
+cd $oldpath
 rm xpc_set_event_stream_handler
 gcc -framework Foundation -o xpc_set_event_stream_handler xpc_set_event_stream_handler.m
 chmod 0755 xpc_set_event_stream_handler
@@ -16,6 +21,6 @@ cp xpc_set_event_stream_handler /usr/local/bin
 # plist install
 launchctl unload /Library/LaunchDaemons/com.ionskvm.plist
 rm /Library/LaunchDaemons/com.ionskvm.plist
-chown root:wheel ./com.ionskvm.plist
 cp ./com.ionskvm.plist /Library/LaunchDaemons
-launchctl load com.ionskvm.plist
+chown root:wheel /Library/LaunchDaemons/com.ionskvm.plist
+launchctl load /Library/LaunchDaemons/com.ionskvm.plist
